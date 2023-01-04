39e41863fc7a1e565971cb3550ddef84df90c2dd
FlyuZ <522893161@qq.com>
2023-01-04 19:38:08 +0800

    修改界面

--- front/src/components/IDmanager.vue
+++ front/src/components/IDmanager.vue
@@ -11,23 +11,37 @@
             </el-header>
             <el-container>
                 <el-aside width="40%" style="padding:10px;">
-                    <el-card shadow="always" style="height: 300px;">
-                        <el-table :data="GalleryInfo" style="width: 100%">
-                            <el-table-column prop="gallery" label="Gallery" />
-                            <el-table-column prop="name" label="Name" />
-                            <el-table-column label="Operations">
-                                <template #default="scope">
-                                    <el-button link type="primary" size="small"
-                                        @click="Detail(scope.$index, scope.row)">查看详情</el-button>
-                                    
-                                </template>
-                            </el-table-column>
-                        </el-table>
+                    <el-card shadow="always" style="height: 400px;">
+                        <el-row>
+                            <el-col :span="24" :gutter="20" style="margin-bottom:20px;">
+                                <el-table :data="GalleryInfo" style="width: 100%; height: 300px;" stripe>
+                                    <el-table-column prop="gallery" label="Gallery" />
+                                    <el-table-column prop="name" label="Name" />
+                                    <el-table-column label="Operations">
+                                        <template #default="scope">
+                                            <el-button link type="primary" size="small"
+                                                @click="Detail(scope.$index, scope.row)">查看详情</el-button>
+
+                                        </template>
+                                    </el-table-column>
+                                </el-table>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="12" :gutter="20" style="margin-bottom:20px;">
+                                <el-button type="primary" @click="register"
+                                    style="width:150px;font-size:16px;letter-spacing:5px;">新增注册</el-button>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-button type="primary" @click="updateGallery"
+                                    style="width:150px;font-size:16px;letter-spacing:5px;">更新检索库</el-button>
+                            </el-col>
+                        </el-row>
                     </el-card>
                 </el-aside>
                 <el-main width="60%" style="padding:10px">
-                    <el-card shadow="always" style="height: 300px;">
-                        <el-table :data="IDInfo" style="width: 100%">
+                    <el-card shadow="always" style="height: 400px;">
+                        <el-table :data="IDInfo" style="width: 100%; height: 360px;" stripe>
                             <el-table-column prop="gallery" label="Gallery" />
                             <el-table-column prop="id" label="ID" />
                             <el-table-column prop="info" label="Info" />
@@ -50,6 +64,7 @@
 </template>
   
 <script lang="ts" setup>
+import { ElLoading, ElMessage, ElMessageBox } from 'element-plus'
 interface ID {
     gallery: string
     id: string
@@ -69,7 +84,48 @@ const Delete = (index: number, row: ID) => {
 const Edit = (index: number, row: ID) => {
     console.log('click')
 }
+const register = () => {
+    console.log('click')
+    ElMessageBox.confirm(
+    '以下新身份将会被注册',
+    '提示',
+    {
+      confirmButtonText: '确认',
+      cancelButtonText: '取消',
+      type: 'warning',
+    }
+  )
+    .then(() => {
+      ElMessage({
+        type: 'success',
+        message: 'Delete completed',
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled',
+      })
+    })
+}
 
+const updateGallery = () => {
+    console.log('click')
+    const loading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+    })
+    setTimeout(() => {
+        loading.close()
+        ElMessage({
+            showClose: true,
+            message: '更新成功.',
+            type: 'success',
+        })
+    }, 2000)
+
+}
 const IDInfo: ID[] = [
     {
         gallery: '104016',
@@ -96,6 +152,41 @@ const IDInfo: ID[] = [
         id: '5',
         info: '',
     },
+    {
+        gallery: '104016',
+        id: '6',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '7',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '8',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '9',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '10',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '11',
+        info: '',
+    },
+    {
+        gallery: '104016',
+        id: '12',
+        info: '',
+    },
 
 ]
 const GalleryInfo: Gallery[] = [
@@ -108,6 +199,14 @@ const GalleryInfo: Gallery[] = [
         name: '环庆养殖场'
     },
     {
+        gallery: '104018',
+        name: '环庆养殖场'
+    },
+    {
+        gallery: '104019',
+        name: '环庆养殖场'
+    },
+    {
         gallery: '102006',
         name: '曹新庄养殖场'
     },
@@ -115,34 +214,32 @@ const GalleryInfo: Gallery[] = [
         gallery: '102007',
         name: '曹新庄养殖场'
     },
+    {
+        gallery: '102008',
+        name: '曹新庄养殖场'
+    },
+    {
+        gallery: '102009',
+        name: '曹新庄养殖场'
+    },
+    {
+        gallery: '1020010',
+        name: '曹新庄养殖场'
+    },
+    {
+        gallery: '102011',
+        name: '曹新庄养殖场'
+    },
 ]
 </script>
 
 <style>
-.left-card-manager {
-padding: 5px;
-  border-radius: 5px;
-  height: 400px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.right-card-manager {
-padding: 5px;
-    border-radius: 5px;
-  height: 400px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 .title {
-  font-size: 40px;
-  font-weight: bold;
-  color: #21b3b9;
-  align-items: center;
-  justify-content: center;
-  letter-spacing: 20px;
+    font-size: 40px;
+    font-weight: bold;
+    color: #21b3b9;
+    align-items: center;
+    justify-content: center;
+    letter-spacing: 20px;
 }
-
 </style>
